cd4cc14a2acf60cff9d0a64f44940046f490a70a
cantthinkofanickname <123295849+cantthinkofanickname@users.noreply.github.com>
2025-08-08 20:03:49 +0300

    Update stbump.bat

--- stbump.bat
+++ stbump.bat
@@ -1,3 +1,12 @@
 @echo off
-title steamtrades bumper
-python main.py
+chcp 65001 >nul
+title Steamtrades bumper
+
+rem Перейти в папку с батником и скриптом
+cd /d "%~dp0"
+
+python st.py
+
+echo.
+echo Нажмите любую клавишу для выхода...
+pause >nul
